5d82945428d78d759d9fc4d1ff2813a4dac1c9a4
kenworthe <kenworthe@gmail.com>
2019-04-04 09:46:50 -0400

    added websockets client and server

---
+++ server/server.rb
@@ -0,0 +1,25 @@
+require 'websocket-eventmachine-server'
+
+EM.run do
+
+  puts "Starting websocket server..."
+
+  WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 8080) do |ws|
+
+  puts "Websocket server started!"
+
+    ws.onopen do
+      puts "Client connected"
+    end
+
+    ws.onmessage do |msg, type|
+      puts "Received message: #{msg}"
+      ws.send msg, :type => type
+    end
+
+    ws.onclose do
+      puts "Client disconnected"
+    end
+  end
+
+end
