2b9cdab351b9dab4850e8ca886ed37fb54ad6e8e
kenworthe <kenworthe@gmail.com>
2019-04-08 09:44:50 -0400

    WIP: deciding on which impl of server to use

--- client/client.rb
+++ client/client.rb
@@ -1,23 +1,49 @@
 require 'websocket-eventmachine-client'
+require 'tty-prompt'
+
+class Client
+  def initalize(server)
+    @ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://0.0.0.0:8080')
+    @server = server
+    @request = nil
+    @response = nil
+    listen
+    send
+  end
+
+  def connect
+    EM.run do
+      @ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://0.0.0.0:8080')
+    end
+  end
+end
+
+@prompt = TTY::Prompt.new
+@ws = nil
+
+def input
+  message = @prompt.ask('What do you want to send?')
+  @ws.send message
+end
 
 EM.run do
 
-  ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://0.0.0.0:8080')
+  @ws = WebSocket::EventMachine::Client.connect(:uri => 'ws://0.0.0.0:8080')
 
-  ws.onopen do
+  @ws.onopen do
     puts "Connected"
   end
 
-  ws.onmessage do |msg, type|
+  @ws.onmessage do |msg, type|
     puts "Received message: #{msg}"
   end
 
-  ws.onclose do |code, reason|
+  @ws.onclose do |code, reason|
     puts "Disconnected with status code: #{code}"
   end
 
   EventMachine.next_tick do
-    ws.send "Hello Server!"
+    @ws.send "Hello Server!"
   end
 
 end
--- server/server.rb
+++ server/server.rb
@@ -1,25 +1,82 @@
+# require 'websocket-eventmachine-server'
+
+# class Server
+#   def initialize
+#     @event_machine = EM
+#     @connections = Hash.new
+#     @rooms = Hash.new
+#     @clients = Hash.new
+#     # @connections[:server] = @server
+#     # @connections[:rooms] = @rooms
+#     # @connections[:clients] = @clients
+#     run
+#   end
+
+#   def run
+#     loop{
+#       Thread.start do |client|
+#         @event_machine.run do
+#           puts "Starting websocket server..."
+#           WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 8080) do |ws|
+
+#             ws.onopen do |handshake|
+#               puts "Client connected"
+#             end
+
+#             ws.onmessage do |msg, type|
+#               puts "Received message: #{msg}"
+#               ws.send msg, :type => type
+#             end
+
+#             ws.onclose do
+#               puts "Client disconnected"
+#             end
+#           end
+#         end
+#       end
+#       puts thread
+#       puts Thread.main
+#     end
+#   }.join
+# end
+
+# Server.new
+
 require 'websocket-eventmachine-server'
 
-EM.run do
-
-  puts "Starting websocket server..."
-
-  WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 8080) do |ws|
-
-  puts "Websocket server started!"
-
-    ws.onopen do
-      puts "Client connected"
-    end
-
-    ws.onmessage do |msg, type|
-      puts "Received message: #{msg}"
-      ws.send msg, :type => type
-    end
-
-    ws.onclose do
-      puts "Client disconnected"
-    end
+class Server
+  def initialize
+    @event_machine = EM.start(@server)
+    @server = WebSocket::EventMachine::Server.start(:host => "0.0.0.0", :port => 8080)
+    @connections = Hash.new
+    @rooms = Hash.new
+    @clients = Hash.new
+    # @connections[:server] = @server
+    # @connections[:rooms] = @rooms
+    # @connections[:clients] = @clients
+    run
   end
 
+  def run
+    loop {
+      puts 'starting server...'
+      Thread.start(@server) do |ws|
+
+        ws.onopen do |handshake|
+          puts "Client connected"
+        end
+
+        ws.onmessage do |msg, type|
+          puts "Received message: #{msg}"
+          ws.send msg, :type => type
+        end
+
+        ws.onclose do
+          puts "Client disconnected"
+        end
+      end
+    }.join
+  end
 end
+
+Server.new
